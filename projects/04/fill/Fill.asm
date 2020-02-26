00c0a529dbcafff128a59620e0b37e0354b2cea8
fletchla <fletchla@gmail.com>
2020-02-25 19:33:07 -0800

    completed project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,38 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+	@8192 // # of RAM registers between SCREEN and KBD
+	D=A //stow 8192 in D
+	@RAMROWS //variable to store how many times to loop through
+	M=D //start with 8192 for how many times to loop
+(RESTART)
+	@index //variable to count up to RAMROWS
+	M=0 //initialize to 0
+(MAIN)
+	@KBD //get input from keyboard
+	D=M //stow value in D
+	@BLACK
+	D;JNE //if not zero, go to BLACK loop. otherwise, continue into WHITE loop
+(WHITE)
+	@index
+	D=M //stow current value of index in D
+	@SCREEN
+	A=A+D //uses index to count up from first SCREEN register
+	M=0 //paint the screen white (default) but ensure all of it is white
+	@END
+	0;JMP //don't proceed through the black loop
+(BLACK)
+	@index
+	D=M //stow current value of index in D
+	@SCREEN
+	A=A+D
+	M=-1 //-1 in 2's complement produces all 1's
+(END)
+	@index
+	MD=M+1 //both M=M+1 and D=M+1. adding 1 to index to count up to 8192
+	@RAMROWS
+	D=D-M //check if it's gone through all RAMROWS yet
+	@RESTART
+	D;JEQ //if this is 0, the entire screen has been painted white or black. Restart to check for new input
+	@MAIN
+	0;JMP //always go back through the main loop if the whole screen isn't filled yet
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,23 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	//takes RAM[0] and adds it to itself in RAM[2] RAM[1] times
+	@2 //RAM[2] is answer register
+	M=0 //initialize to 0
+(LOOP)
+	@1
+	D=M
+	@END
+	D;JEQ
+	@0
+	D=M
+	@2 //in RAM[2]
+	M=M+D //add RAM[0] to the answer
+	@1
+	M=M-1
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
+	
